d4494daaa83ce48a7527ec7e7968b48a59d8b752
Realnetboy <dicksonjd60@gmail.com.com>
2022-09-09 05:37:25 -0700

    task 4 to 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#inculde <stdio.h>
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+} 
