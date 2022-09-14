cb34e06e8270a62b33c94d460f22b6ae8710f6ff
Alcott <104767359+Realnetboy@users.noreply.github.com>
2022-09-14 11:12:16 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,5 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
