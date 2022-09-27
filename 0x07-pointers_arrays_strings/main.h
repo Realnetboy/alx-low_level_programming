0469c28da49f4b40649b1386270e1923c3053a9d
Alcott <104767359+Realnetboy@users.noreply.github.com>
2022-09-27 22:47:28 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,7 +5,8 @@
 * void prototypes()
 * int prototypes()
 */
-int_putchar(char);
+
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -15,3 +16,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif /*MAIN_H*/
