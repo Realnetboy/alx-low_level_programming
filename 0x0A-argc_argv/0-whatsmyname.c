7da93f5ad13b48c5681a82149e02fff6e8f62935
Alcott <104767359+Realnetboy@users.noreply.github.com>
2022-10-01 20:28:14 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,10 @@
 
 /**
  * main - prints the name of the program
+ * if you rename the program, it will print the new name, without having to 
+ * compile it again
+ * you should not remove the path before the name of the program
+ *
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
@@ -10,5 +14,6 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
