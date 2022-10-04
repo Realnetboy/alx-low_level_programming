c60ed6a6cca1570fa7c1549690dab177396fb58f
Alcott <104767359+Realnetboy@users.noreply.github.com>
2022-10-04 14:09:54 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,13 +3,20 @@
 
 #include <stdlib.h>
 
-void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
-char *strdup(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char *strtow(char *str);
 
 #endif /* MAIN_H */
+
+
+
+
+
+
+
