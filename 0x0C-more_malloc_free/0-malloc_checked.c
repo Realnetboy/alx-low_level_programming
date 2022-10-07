c0c69e056db334df6bf0741e49d5289921390220
Alcott <104767359+Realnetboy@users.noreply.github.com>
2022-10-07 18:50:54 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /**
-  * malloc_check - allocates memory using malloc
-  * @b: memory to allocate
-  *
-  * Return: pointer to allocated memory or normal process termination if error
-  */
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
+ */
 void *malloc_checked(unsigned int b)
 {
-        void *p = malloc(b);
-   
-        if (p == NULL)
-                exit(98);
-        return (p);
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
